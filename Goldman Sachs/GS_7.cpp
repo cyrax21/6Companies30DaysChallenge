aa10aaca0756bb86eddcb25f8d73939e6bcb346e
Joy deb <77791817+cyrax21@users.noreply.github.com>
2022-01-02 20:02:25 +0530

    Update GS_7.cpp

--- Goldman Sachs/GS_7.cpp
+++ Goldman Sachs/GS_7.cpp
@@ -1,1 +1,34 @@
-GCD_7
+// { Driver Code Starts
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution {
+  public:
+    int findPosition(int N , int M , int K) {
+        // code here
+        // if ans is 0 that mean it is indicating last position 
+        int ans = (M+K-1)%(N);
+        if(ans) return ans;
+        return N;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,M,K;
+        
+        cin>>N>>M>>K;
+
+        Solution ob;
+        cout << ob.findPosition(N,M,K) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
